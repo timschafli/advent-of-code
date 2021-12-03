6ad0fabb35c9bb2cc940f6754b674ec8e0509721
Tim Schafli <tim.schafli@zapier.com>
2021-12-02 23:25:36 -0800

    Solved Day 3

--- solutions/2021/day_3/README.md
+++ solutions/2021/day_3/README.md
@@ -1,8 +1,32 @@
 # Day 3 (2021)
 
-`TITLE` ([prompt](https://adventofcode.com/2021/day/3))
+`Binary Diagnostic` ([prompt](https://adventofcode.com/2021/day/3))
 
 ## Part 1
 
+We need to do a few things:
+
+- check all the bits in each column, and return the most common / least common bits.
+- create two outputs:
+- - gamma rate from most common bits
+- - epsilon rate from least common bits
+- convert the two rates from binary to an decimal number (integer)
+- multiply the rates together for the answer
+
 ## Part 2
 
+We need to find two new values:
+
+- oxygen generator rating
+- - bit criteria
+- - - most common value (0 or 1) in the current bit position
+- - - keep only numbers with that bit in that position
+- - - 0 and 1 equal, keep 1
+- CO2 scrubber rating
+- - opposite of oxygen
+
+To find these,
+
+1. consider just the first bit of each number
+2. use bit critera to remove numbers in the list until only 1 is left - that is the value
+3.
--- solutions/2021/day_3/solution.py
+++ solutions/2021/day_3/solution.py
@@ -1,19 +1,87 @@
 # prompt: https://adventofcode.com/2021/day/3
 
-from ...base import TextSolution, answer
+from copy import deepcopy
+from collections import Counter
+from ...base import StrSplitSolution, answer
+
 # from typing import Tuple
 
-class Solution(TextSolution):
+
+class Solution(StrSplitSolution):
     _year = 2021
     _day = 3
 
-    # @answer(1234)
-    def part_1(self) -> int:
-        pass
+    def create_columns(inputLines):
+        outputLines = []
+        for inputLine in inputLines:
+            outputLines.append([char for char in inputLine])
+        return outputLines
 
-    # @answer(1234)
+    def get_average_for_columns(inputLines):
+        columns = []
+        for line in inputLines:
+            for index, bit in enumerate(line):
+                if len(columns) <= index:
+                    columns.append([])
+                columns[index].append(bit)
+        commonLines = [Counter(element).most_common(2) for element in columns]
+        # for list comprehensions below, we need to make sure that commonLines
+        # Counter.mostCommon entries do have 2 Tuples each after some columns
+        # only have 1 type of bit:
+        for line in commonLines:
+            if len(line) == 1:
+                line.append([None, None])
+        mostCommon = [element[0][0] or None for element in commonLines]
+        leastCommon = [element[1][0] or None for element in commonLines]
+        equalNumber = [element[0][1] == element[1][1] for element in commonLines]
+        return [mostCommon, leastCommon, equalNumber]
+
+    @answer(1307354)
+    def part_1(self) -> int:
+        input = Solution.create_columns(self.input)
+        mostCommon, leastCommon, _ = Solution.get_average_for_columns(input)
+        gamma_rate, epsilon_rate = ["".join(mostCommon), "".join(leastCommon)]
+        return int(gamma_rate, 2) * int(epsilon_rate, 2)
+
+    @answer(482500)
     def part_2(self) -> int:
-        pass
+        inputOxygen = Solution.create_columns(self.input)
+        inputCO2 = Solution.create_columns(self.input)
+        # find oxygen generator rating
+
+        def removeLeastCommon(input, bitIndex: int):  # remove least common
+            """
+            Remove lines with least common bit for bit index.
+            Last remaining line = oxygen generator rating
+            """
+            mostCommon, _, equalNumber = Solution.get_average_for_columns(input)
+            bitToKeep = mostCommon[bitIndex]
+            if equalNumber[bitIndex] == True:
+                bitToKeep = "1"
+            return [line for line in input if line[bitIndex] == bitToKeep]
+
+        def removeMostCommon(input, bitIndex: int):  # remove least common
+            """
+            Remove lines with most common bit for bit index.
+            Last remaining line = CO2 generator rating
+            """
+            _, leastCommon, equalNumber = Solution.get_average_for_columns(input)
+            bitToKeep = leastCommon[bitIndex]
+            if equalNumber[bitIndex] == True:
+                bitToKeep = "0"
+            return [line for line in input if line[bitIndex] == bitToKeep]
+
+        bitIndex = 0
+        while len(inputOxygen) > 1:
+            inputOxygen = removeLeastCommon(deepcopy(inputOxygen), bitIndex=bitIndex)
+            bitIndex += 1
+
+        bitIndex = 0
+        while len(inputCO2) > 1:
+            inputCO2 = removeMostCommon(deepcopy(inputCO2), bitIndex=bitIndex)
+            bitIndex += 1
+
+        return int("".join(inputOxygen[0]), 2) * int("".join(inputCO2[0]), 2)
 
     # def solve(self) -> Tuple[int, int]:
     #     pass

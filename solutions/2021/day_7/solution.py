dabd8db10b37037e74f4e07082bd8a1f91fd677f
Tim Schafli <tim.schafli@zapier.com>
2021-12-06 21:43:15 -0800

    Solved day 7

--- solutions/2021/day_7/solution.py
+++ solutions/2021/day_7/solution.py
@@ -1,19 +1,51 @@
 # prompt: https://adventofcode.com/2021/day/7
 
-from ...base import TextSolution, answer
+from collections import Counter
+from copy import copy
+from ...base import IntSplitSolution, answer
 # from typing import Tuple
 
-class Solution(TextSolution):
+class Solution(IntSplitSolution):
     _year = 2021
     _day = 7
+    separator = ","
 
-    # @answer(1234)
+    @answer(340987)
     def part_1(self) -> int:
-        pass
+        crabs = self.input
+        cheapest_fuel = -1
+        print(range(max(set(self.input)) + 1))
+        for align_pos in range(max(set(self.input)) + 1):
+            fuel = 0
+            for i in crabs:
+                if i < align_pos:
+                    fuel += (align_pos - i)
+                elif i > align_pos:
+                    fuel += (i - align_pos)
+            if fuel < cheapest_fuel or cheapest_fuel == -1:
+                cheapest_fuel = fuel
+        return cheapest_fuel
 
-    # @answer(1234)
+    @answer(96987874)
     def part_2(self) -> int:
-        pass
+        initial_crabs = self.input
+        cheapest_fuel = -1
+        for align_pos in range(max(set(self.input)) + 1):
+            crabs = copy(initial_crabs)
+            fuel = 0
+            for i in crabs:
+                fuel_cost_counter = 1
+                while i < align_pos:
+                    i += 1
+                    fuel += fuel_cost_counter
+                    fuel_cost_counter += 1
+                while i > align_pos:
+                    i -= 1
+                    fuel += fuel_cost_counter
+                    fuel_cost_counter += 1
+            if fuel < cheapest_fuel or cheapest_fuel == -1:
+                cheapest_fuel = fuel
+        return cheapest_fuel
 
     # def solve(self) -> Tuple[int, int]:
     #     pass

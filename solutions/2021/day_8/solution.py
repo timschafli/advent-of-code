fc86f670573d02b12908f4deae80b95153acbc0a
Tim Schafli <tim.schafli@zapier.com>
2021-12-08 12:45:25 -0800

    Init day 8

---
+++ solutions/2021/day_8/solution.py
@@ -0,0 +1,19 @@
+# prompt: https://adventofcode.com/2021/day/8
+
+from ...base import TextSolution, answer
+# from typing import Tuple
+
+class Solution(TextSolution):
+    _year = 2021
+    _day = 8
+
+    # @answer(1234)
+    def part_1(self) -> int:
+        pass
+
+    # @answer(1234)
+    def part_2(self) -> int:
+        pass
+
+    # def solve(self) -> Tuple[int, int]:
+    #     pass

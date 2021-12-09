48c038c497cab1ebde1d544de0b140d04c679f66
Tim Schafli <tim.schafli@zapier.com>
2021-12-08 18:11:23 -0800

    Solved day 1

--- solutions/2021/day_8/README.md
+++ solutions/2021/day_8/README.md
@@ -1,8 +1,9 @@
 # Day 8 (2021)
 
-`TITLE` ([prompt](https://adventofcode.com/2021/day/8))
+`Seven Segment Search` ([prompt](https://adventofcode.com/2021/day/8))
 
 ## Part 1
 
 ## Part 2
 
+Use sets and subset
--- solutions/2021/day_8/solution.py
+++ solutions/2021/day_8/solution.py
@@ -1,18 +1,30 @@
 # prompt: https://adventofcode.com/2021/day/8
 
-from ...base import TextSolution, answer
+from ...base import StrSplitSolution, answer
 # from typing import Tuple
 
-class Solution(TextSolution):
+EASY_DIGIT_CONVERSION = {
+    2: 1,
+    4: 4,
+    3: 7,
+    7: 8
+}
+
+class Solution(StrSplitSolution):
     _year = 2021
     _day = 8
 
-    # @answer(1234)
+    # @answer(367)
     def part_1(self) -> int:
-        pass
+        total = 0
+        for line in self.input:
+            _, wires = line.split(" | ")
+            total += sum([1 for x in wires.split(" ") if len(x) in EASY_DIGIT_CONVERSION])
+        return total
 
     # @answer(1234)
     def part_2(self) -> int:
+        
         pass
 
     # def solve(self) -> Tuple[int, int]:

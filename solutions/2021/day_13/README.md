976b84d3e4f3031f9048bc619851e77e8a439082
Tim Schafli <tim.schafli@zapier.com>
2021-12-12 22:36:37 -0800

    Solved Day 13 part 1

--- solutions/2021/day_13/README.md
+++ solutions/2021/day_13/README.md
@@ -1,8 +1,7 @@
 # Day 13 (2021)
 
-`TITLE` ([prompt](https://adventofcode.com/2021/day/13))
+`Transparent Origami` ([prompt](https://adventofcode.com/2021/day/13))
 
 ## Part 1
 
 ## Part 2
-
--- solutions/2021/day_13/solution.py
+++ solutions/2021/day_13/solution.py
@@ -1,15 +1,65 @@
 # prompt: https://adventofcode.com/2021/day/13
 
-from ...base import TextSolution, answer
-# from typing import Tuple
+from copy import copy
+from ...base import StrSplitSolution, answer
 
-class Solution(TextSolution):
+# from typing import Tuple
+AXI = {"x": 0, "y": 1}
+
+
+def parse_input(input):
+    dots = set()
+    fold_instructions = []
+    for line in input:
+        if line.startswith("fold along"):
+            fold_instructions.append(
+                {"axis": line.split("=")[0][-1], "coord": int(line.split("=")[1])}
+            )
+        elif line != "":
+            x, y = line.split(",")
+            dots.add((int(x), int(y)))
+    return dots, fold_instructions
+
+
+def get_max(x_or_y, dots):
+    return max([a[AXI[x_or_y]] for a in dots])
+
+
+class Solution(StrSplitSolution):
     _year = 2021
     _day = 13
 
-    # @answer(1234)
+    # @answer(745)
     def part_1(self) -> int:
-        pass
+        dots, fold_instructions = parse_input(self.input)
+        print(len(dots))
+        for fold in fold_instructions[0:1]:
+            max = get_max(fold["axis"], dots)
+            assert (
+                len([dot for dot in dots if dot[AXI[fold["axis"]]] == fold["coord"]])
+                == 0
+            )  # fold lines should never contain dots
+            assert (
+                fold["coord"] < (max // 2) + 1
+            )  # assuming we don't need to fold up past 0
+
+            for dot in copy(dots):
+                if dot[AXI[fold["axis"]]] > fold["coord"]:
+                    new_dot = (0, 0)
+                    if fold["axis"] == "x":
+                        new_dot = (
+                            dot[AXI["x"]] - (dot[AXI["x"]] - fold["coord"]) * 2,
+                            dot[AXI["y"]],
+                        )
+                    if fold["axis"] == "y":
+                        new_dot = (
+                            dot[AXI["x"]],
+                            dot[AXI["y"]] - (dot[AXI["y"]] - fold["coord"]) * 2,
+                        )
+                    dots.add(new_dot)
+                    dots.remove(dot)
+
+        return len(dots)
 
     # @answer(1234)
     def part_2(self) -> int:

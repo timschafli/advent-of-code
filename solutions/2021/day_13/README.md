47aaf9c4a9b5777ae62393b2d228eb86518a674e
Tim Schafli <tim.schafli@zapier.com>
2021-12-12 21:37:06 -0800

    Init 13

---
+++ solutions/2021/day_13/README.md
@@ -0,0 +1,8 @@
+# Day 13 (2021)
+
+`TITLE` ([prompt](https://adventofcode.com/2021/day/13))
+
+## Part 1
+
+## Part 2
+
---
+++ solutions/2021/day_13/solution.py
@@ -0,0 +1,19 @@
+# prompt: https://adventofcode.com/2021/day/13
+
+from ...base import TextSolution, answer
+# from typing import Tuple
+
+class Solution(TextSolution):
+    _year = 2021
+    _day = 13
+
+    # @answer(1234)
+    def part_1(self) -> int:
+        pass
+
+    # @answer(1234)
+    def part_2(self) -> int:
+        pass
+
+    # def solve(self) -> Tuple[int, int]:
+    #     pass

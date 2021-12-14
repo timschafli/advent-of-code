aab6883e994f64061b0dedbf0ced6dc93833143b
Tim Schafli <tim.schafli@zapier.com>
2021-12-13 21:51:22 -0800

    Init day 14

---
+++ solutions/2021/day_14/README.md
@@ -0,0 +1,7 @@
+# Day 14 (2021)
+
+`Extended Polymerization` ([prompt](https://adventofcode.com/2021/day/14))
+
+## Part 1
+
+## Part 2
---
+++ solutions/2021/day_14/solution.py
@@ -0,0 +1,21 @@
+# prompt: https://adventofcode.com/2021/day/14
+
+from ...base import StrSplitSolution, answer
+
+# from typing import Tuple
+
+
+class Solution(StrSplitSolution):
+    _year = 2021
+    _day = 14
+
+    # @answer(1234)
+    def part_1(self) -> int:
+        pass
+
+    # @answer(1234)
+    def part_2(self) -> int:
+        pass
+
+    # def solve(self) -> Tuple[int, int]:
+    #     pass

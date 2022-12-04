d5e3fc225460c889411d85c2d8e9223aeed77f88
Tim Schafli <tim.schafli@zapier.com>
2022-12-03 21:16:17 -0800

    Completed day 4

--- solutions/2022/day_4/README.md
+++ solutions/2022/day_4/README.md
@@ -1,8 +1,7 @@
 # Day 4 (2022)
 
-`TITLE` ([prompt](https://adventofcode.com/2022/day/4))
+`Camp Cleanup` ([prompt](https://adventofcode.com/2022/day/4))
 
 ## Part 1
 
 ## Part 2
-
--- solutions/2022/day_4/solution.py
+++ solutions/2022/day_4/solution.py
@@ -1,19 +1,38 @@
 # prompt: https://adventofcode.com/2022/day/4
 
-from ...base import TextSolution, answer
+from ...base import StrSplitSolution, answer
+
 # from typing import Tuple
 
-class Solution(TextSolution):
+
+def get_range(elf_range):
+    range_start, range_end = elf_range.split("-")
+    return set(range(int(range_start), int(range_end) + 1))
+
+
+class Solution(StrSplitSolution):
     _year = 2022
     _day = 4
 
-    # @answer(1234)
+    @answer(536)
     def part_1(self) -> int:
-        pass
+        fully_duplicated_work_counts = 0
+        pairs = self.input
+        for pair in pairs:
+            ranges = [get_range(elf_range) for elf_range in pair.split(",")]
+            if ranges[0].issubset(ranges[1]) or ranges[1].issubset(ranges[0]):
+                fully_duplicated_work_counts += 1
+        return fully_duplicated_work_counts
 
-    # @answer(1234)
+    @answer(845)
     def part_2(self) -> int:
-        pass
+        partially_duplicated_work_counts = 0
+        pairs = self.input
+        for pair in pairs:
+            ranges = [get_range(elf_range) for elf_range in pair.split(",")]
+            if len(ranges[0].intersection(ranges[1])) > 0:
+                partially_duplicated_work_counts += 1
+        return partially_duplicated_work_counts
 
     # def solve(self) -> Tuple[int, int]:
     #     pass

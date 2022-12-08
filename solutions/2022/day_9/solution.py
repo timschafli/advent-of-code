923c6d0954c45c2835ed5213da55611d9512c429
Tim Schafli <tim.schafli@zapier.com>
2022-12-08 11:53:24 -0800

    Init day9

---
+++ solutions/2022/day_9/solution.py
@@ -0,0 +1,19 @@
+# prompt: https://adventofcode.com/2022/day/9
+
+from ...base import TextSolution, answer
+# from typing import Tuple
+
+class Solution(TextSolution):
+    _year = 2022
+    _day = 9
+
+    # @answer(1234)
+    def part_1(self) -> int:
+        pass
+
+    # @answer(1234)
+    def part_2(self) -> int:
+        pass
+
+    # def solve(self) -> Tuple[int, int]:
+    #     pass

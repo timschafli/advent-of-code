d4374affa43d43d6d8d8e0b264da30bc587e1586
Tim Schafli <tim.schafli@zapier.com>
2022-12-10 18:57:13 -0800

    Complete day 9

--- solutions/2022/day_9/solution.py
+++ solutions/2022/day_9/solution.py
@@ -2,15 +2,13 @@
 
 from ...base import StrSplitSolution, answer
 
-# from typing import Tuple
+from typing import Tuple
 
 
 class Rope:
     def __init__(self, knots):
         assert knots > 1
         self.rope = [(0, 0) for _ in range(knots)]
-        # self.head = (0, 0)
-        # self.tail = (0, 0)
         self.tail_log = set([self.rope[-1]])
         self.max_tail_slack = 0
 
@@ -32,69 +30,65 @@ class Rope:
         if direction == "D":
             return (0, amount)
 
-    def move_tail_closer(self):
+    def move_tail_closer(self, knot, prev_knot, rope_index):
         # X
-        x = self.rope[-1][0]
-        y = self.rope[-1][1]
-        x = x - 1 if self.head[0] < x else x
-        x = x + 1 if self.head[0] > x else x
-        y = y - 1 if self.head[1] < y else y
-        y = y + 1 if self.head[1] > y else y
+        x = knot[0]
+        y = knot[1]
+        xH = prev_knot[0]
+        yH = prev_knot[1]
+        x = x - 1 if xH < x else x
+        x = x + 1 if xH > x else x
+        y = y - 1 if yH < y else y
+        y = y + 1 if yH > y else y
 
-        self.rope[-1] = (x, y)
+        self.rope[rope_index] = (x, y)
 
-    def should_move_tail(self):
+    def should_move_knot(self, knot, prev_knot):
+        x = knot[0]
+        y = knot[1]
+        xH = prev_knot[0]
+        yH = prev_knot[1]
         # X
-        if self.head[0] < self.rope[-1][0] - (self.max_tail_slack + 1):
+        if x < xH - (self.max_tail_slack + 1):
             return True
-        if self.head[0] > self.rope[-1][0] + (self.max_tail_slack + 1):
+        if x > xH + (self.max_tail_slack + 1):
             return True
         # Y
-        if self.head[1] < self.rope[-1][1] - (self.max_tail_slack + 1):
+        if y < yH - (self.max_tail_slack + 1):
             return True
-        if self.head[1] > self.rope[-1][1] + (self.max_tail_slack + 1):
+        if y > yH + (self.max_tail_slack + 1):
             return True
         return False
 
     def log_tail(self):
         self.tail_log.add(self.rope[-1])
 
+    def update_knots(self):
+        for index in range(len(self.rope) - 1):
+            if self.should_move_knot(self.rope[index + 1], self.rope[index]):
+                self.move_tail_closer(self.rope[index + 1], self.rope[index], index + 1)
+
     def move_head(self, direction, amount):
-        # print(direction)
-        # print(amount)
-        # calculate tuple of movement from direction
         for _ in range(amount):
             move = self.get_movement_coords(direction, 1)
-            # add current head tuple and movement tuple together
             self.head = (self.head[0] + move[0], self.head[1] + move[1])
-            if self.should_move_tail():
-                self.move_tail_closer()
+            self.update_knots()
             self.log_tail()
-            # print(f"Head: {self.head} Tail: {self.tail}")
 
 
 class Solution(StrSplitSolution):
     _year = 2022
     _day = 9
 
-    @answer(6181)
-    def part_1(self) -> int:
-        rope = Rope(2)
+    @answer((6181, 2386))
+    def solve(self) -> Tuple[int, int]:
+        rope1 = Rope(2)
+        rope2 = Rope(10)
         moves = [
             {"direction": move.split(" ")[0], "amount": int(move.split(" ")[1])}
             for move in self.input
         ]
-        # print(f"Head: {rope.head} Tail: {rope.tail}")
-
         for move in moves:
-            rope.move_head(move["direction"], move["amount"])
-        # print(rope.tail_log)
-        return len(rope.tail_log)
-
-    # @answer(1234)
-    def part_2(self) -> int:
-        rope = Rope(10)
-        pass
-
-    # def solve(self) -> Tuple[int, int]:
-    #     pass
+            rope1.move_head(move["direction"], move["amount"])
+            rope2.move_head(move["direction"], move["amount"])
+        return (len(rope1.tail_log), len(rope2.tail_log))
